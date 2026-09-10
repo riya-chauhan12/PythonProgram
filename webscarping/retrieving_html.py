70176b15bc7f505b2ba5d19df702b1ae022c623f
riya-chauhan12 <riyachauhan1853@gmail.com>
2026-09-10 22:01:49 +0530

    refactor: update web scraping logic and improve quote extraction

--- webscarping/retrieving_html.py
+++ webscarping/retrieving_html.py
@@ -1,28 +1,79 @@
 
 #using request method
 # fake_useragent so it does not block the scraping
-import requests
-import time
-from fake_useragent import UserAgent
-url="https://www.flipkart.com/all/~cs-6ef68bc8d283b86730515a8f2c87ff23/pr?sid=0pm%2Cfcn%2C821%2Ca7x%2C2si&marketplace=FLIPKART&restrictLocale=true&BU=Mixed"
 
-session=requests.Session()
-headers={
-    'User-Agent': UserAgent().random,
-    'Accept-Language': 'en-Us,en;q=0.9',
-    'Accept-Encoding':'gzip,deflate,br',
-    'Connection':'keep-alive',
-    'Referer' :'https://www.google.com'
-}
+#using request method
+# fake_useragent so it does not block the scraping
+#proxy_auth='HhUWuCeRDDLdFmbWUuu1KkpVP5biGdUDPmogehmjkRUN'
+#==================Basic request (send http Request)===================
+'''
+import request
+url='https://www.geeksforgeeks.org/python/python-programming-language-tutorial/'
 
-proxy_auth='HhUWuCeRDDLdFmbWUuu1KkpVP5biGdUDPmogehmjkRUN'
-proxies={
-    'http': f'http://{proxy_auth}',
-    'https':f'https://{proxy_auth}'
-}
-
-time.sleep(2)
-r=session.get(url,proxies=proxies,headers=headers)
-print(r.text)
+#Sends a GET request to the given URL.
+res=requests.get(url)
+#Returns the raw HTML of the page in bytes.
+print(res.content)
+#returns http status code
+print(res.status_code)
 with open("file.html","w") as f:
-    f.write(r.text)
+    f.write(res.text) '''
+
+#===================Parsing with BeautifulSoup================
+'''
+import requests
+from bs4 import BeautifulSoup
+url='https://www.geeksforgeeks.org/python/python-programming-language-tutorial/'
+res=requests.get(url)
+#Converts HTML into a searchable object. 'html.parser' is the built-in parse
+soup=BeautifulSoup(res.content,'html.parser')
+#Formats the HTML nicely for easier reading
+#print(soup.prettify())
+
+with open("bs$.html","w" )as f:
+    f.write(soup.prettify())
+'''
+#====================Extracting Content by Tag and Class========
+import requests
+from bs4 import BeautifulSoup
+url='https://www.geeksforgeeks.org/python/python-programming-language-tutorial/'
+res=requests.get(url)
+soup=BeautifulSoup(res.content,'html.parser')
+quotes=[]
+
+#Find every <div> whose class is this particular class.
+quote_box=soup.find_all('div',class_='col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top')
+
+for box in quote_box:
+    #Inside this quote box, find the <img> element.alt is atrribute of img tag
+    quote_text=box.img['alt'].split("#")
+    #creating one dictionary containing all the information about one quote.
+    quote={
+        #finds the <h5>.gets its text.removes unnecessary whitespace around it.
+        'theme':box.h5.text.strip(),
+        'image_url':box.img['src'],
+        #so we split the data by ('#') now this gives the data in 1st index
+        'lines':quote_text[0],
+        #Because not every quote may necessarily contain:author
+        'author':quote_text[1] if len(quote_text)> 1 else 'Unknown'
+
+    }
+    quotes.append(quote)
+#diplay quotes
+for q in quotes[:5]: #print only 5
+    print(q)
+
+'''
+This entire <div> is one quote box.
+
+The scraper takes this one box: and extract information
+
+<html>
+    <body>
+        <div class="quote">
+            ...
+        </div>
+    </body>
+</html>
+
+'''
